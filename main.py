e5446932734b1cd9323da3bd96315530f9f58a3e
Colin Tinsman <tinsmancb@gmail.com>
2024-03-06 14:01:37 -0500

    Basic LCG

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+class lcg:
+    def __init__(self, seed, a=1, c=0, m=2**64-1):
+        self.seed = seed
+        self.a = a
+        self.c = c
+        self.m = m
+        self.current_val = seed
+        self.counter = 0
+
+    def roll(self):
+        self.current_val = (self.a*self.current_val + self.c) % self.m
+        self.counter += 1
+        return self.current_val
+
+
 def main():
-    pass
+    prng = lcg(3453252345, 723325234)
+    for _ in range(10):
+        print(prng.roll())
 
 
 if __name__ == '__main__':
